634e4dc439f8c88a812041923161439824e58dec
unknown <moazzemh222@gmail.com>
2025-07-27 11:05:36 +0600

    First Commit

---
+++ banglaCrud/banglaCrud/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace banglaCrud.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+    }
+}
---
+++ banglaCrud/banglaCrud/Models/House.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace banglaCrud.Models
+{
+    public class House
+    {
+        [Key]
+        public int houseId { get; set; }
+        public string HouseName { get; set; }
+        public int price { get; set; }
+        public int sqFeet { get; set; }
+    }
+}
